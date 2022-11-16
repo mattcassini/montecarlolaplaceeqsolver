bc59d7cd33b2a18fb8a690e8d06fc443e94b6ffa
Matt <mc225@njit.edu>
2022-11-16 16:00:24 -0500

    finished fully vectorising

--- tour_du_wino.m
+++ tour_du_wino.m
@@ -1,6 +1,6 @@
 clear;clc;close all;
-m = 2^4; %number of trials for each point
-n = 2^4; %size of grid
+m = 2^6; %number of trials for each point
+n = 2^6; %size of grid
 k = (n-2)^2; %interior pts
 s = RandStream("dsfmt19937"); %seed for random number gen
 %x = linspace(0,1,n);
@@ -10,42 +10,42 @@ reward = zeros(n-2,n-2);
 
 g = zeros(n,n);
 %input BCs
-g(1,:) = 0; %top
-g(n,:) = 0; %bottom
+g(1,:) = 1; %top
+g(n,:) = 1; %bottom
 g(:,1) = 0; %left
 g(:,n) = 0; %right
+u = g;
 g = repmat(g,1,1,m);
 %g = sub2ind(g,n,n);
 A = 1:k;
 %figure()
 %hold on
-pos = zeros(n-2,n-2,m,2);
-walk = pos;
-final_pos = pos;
+pos1 = repmat((2:n-1)',1,n-2,m);
+pos2 = permute(pos1,[2 1 3]);
+final_pos1 = zeros(n-2,n-2,m);
+final_pos2 = final_pos1;
 j = 0;
 
-while sum(final_pos(:,:,:)==0) >= 1
+while sum(final_pos1(:)==0) >= 1
     randnum = rand(s,[n-2 n-2 m]);
-    walk = zeros(n-2,n-2,m,2);
-    walk(randnum<=0.25,2) = -1; %left
-    walk((randnum>0.25 & randnum<=0.5),2) = 1; %right
-    walk(randnum>0.5 & randnum<=0.75,1) = -1; %up
-    walk(randnum>0.75,1) = 1; %down
-    pos = pos + walk;
-    %if j >= 1
-     %   pos(:,pos(1,:)==1 | pos(1,:)==n | pos(2,:)==1 | pos(2,:)==n) = final_pos(:,pos(1,:)==1 | pos(1,:)==n | pos(2,:)==1 | pos(2,:)==n);
-    %end
-    pos(not(final_pos==0)) = 0;
-    final_pos(pos(:,:,:,1)==1 | pos(:,:,:,1)==n | pos(:,:,:,2)==1 | pos(:,:,:,2)==n,:) = pos(:,:,:,1)==1 | pos(:,:,:,1)==n | pos(:,:,:,2)==1 | pos(:,:,:,2)==n;
-    %walk(:,pos(1,:)==1 | pos(1,:)==n | pos(2,:)==1 | pos(2,:)==n) = [];
-    %pos(:,pos(1,:)==1  | pos(1,:)==n | pos(2,:)==1 | pos(2,:)==n) = [];
-    %m_ = length(pos);
+    walk1 = zeros(n-2,n-2,m);
+    walk2 = walk1;
+    walk1(randnum<=0.25) = -1; %up
+    walk1((randnum>0.25 & randnum<=0.5)) = 1; %down
+    walk2(randnum>0.5 & randnum<=0.75) = -1; %left
+    walk2(randnum>0.75) = 1; %right
+    pos1 = pos1 + walk1;
+    pos2 = pos2 + walk2;
+    pos1(not(final_pos1==0)) = 0;
+    pos2(not(final_pos2==0)) = 0;
+    final_pos1(pos1==1 | pos1==n | pos2==1 | pos2==n) = pos1(pos1==1 | pos1==n | pos2==1 | pos2==n);
+    final_pos2(pos1==1 | pos1==n | pos2==1 | pos2==n) = pos2(pos1==1 | pos1==n | pos2==1 | pos2==n);
     j = j + 1;
 end
-R_avg = sum(sum(g(final_pos(:,:,:,1),final_pos(:,:,:,2))))/m;
-%pause(1)
-%close all;
+R_avg = sum(g(sub2ind([n n m],final_pos1,final_pos2)),3)/m;
 
-u = g;
 u(2:n-1,2:n-1) = R_avg;
+mesh(u)
 
+load gong
+sound(y,Fs)

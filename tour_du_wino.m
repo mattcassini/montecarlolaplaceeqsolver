1d97a847dbedc18d170d9dd9dd0d7f53bf724b60
Matt <mc225@njit.edu>
2022-11-17 14:46:25 -0500

    saved some solutions,increased efficiency very slightly

--- tour_du_wino.m
+++ tour_du_wino.m
@@ -5,8 +5,6 @@ k = (n-2)^2; %interior pts
 s = RandStream("dsfmt19937"); %seed for random number gen
 %x = linspace(0,1,n);
 %y = x;
-%[X,Y] = meshgrid(x,y);
-reward = zeros(n-2,n-2);
 
 g = zeros(n,n);
 %input BCs
@@ -15,15 +13,14 @@ g(n,:) = 1; %bottom
 g(:,1) = 0; %left
 g(:,n) = 0; %right
 u = g;
-g = repmat(g,1,1,m);
-%g = sub2ind(g,n,n);
-A = 1:k;
-%figure()
-%hold on
+%g = repmat(g,1,1,m);
+
 pos1 = repmat((2:n-1)',1,n-2,m);
 pos2 = permute(pos1,[2 1 3]);
+
 final_pos1 = zeros(n-2,n-2,m);
 final_pos2 = final_pos1;
+
 j = 0;
 
 while sum(final_pos1(:)==0) >= 1
@@ -42,7 +39,7 @@ while sum(final_pos1(:)==0) >= 1
     final_pos2(pos1==1 | pos1==n | pos2==1 | pos2==n) = pos2(pos1==1 | pos1==n | pos2==1 | pos2==n);
     j = j + 1;
 end
-R_avg = sum(g(sub2ind([n n m],final_pos1,final_pos2)),3)/m;
+R_avg = sum(g(sub2ind([n n],final_pos1,final_pos2)),3)/m;
 
 u(2:n-1,2:n-1) = R_avg;
 mesh(u)

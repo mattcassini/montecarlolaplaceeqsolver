b34683029103b0ab7f9d38521326e1c6d6c5cf43
Matt <mc225@njit.edu>
2022-11-17 21:43:25 -0500

    cleaning

--- tour_du_wino.m
+++ tour_du_wino.m
@@ -1,5 +1,5 @@
 %Monte Carlo PDE Solver
-%Math450H
+%M450H
 
 clear;clc;close all;
 m = 2^6; %number of trials for each point
@@ -16,7 +16,6 @@ g(n,:) = 1; %bottom
 g(:,1) = 0; %left
 g(:,n) = 0; %right
 u = g;
-%g = repmat(g,1,1,m);
 
 pos1 = repmat((2:n-1)',1,n-2,m);
 pos2 = permute(pos1,[2 1 3]);
@@ -24,11 +23,6 @@ pos1 = pos1(:);
 pos2 = pos2(:);
 pos1 = repmat(pos1,1,2);
 pos2 = repmat(pos2,1,2);
-
-final_pos1 = zeros(n-2,n-2,m);
-final_pos1 = final_pos1(:);
-final_pos2 = final_pos1;
-final_pos2 = final_pos2(:);
 
 reward = zeros(n,n);
 j = 0;
@@ -44,8 +38,6 @@ while size(pos1,1) >= 1
     pos1(:,1) = pos1(:,1) + walk1;
     pos2(:,1) = pos2(:,1) + walk2;
     z = pos1(:,1)==1 | pos1(:,1)==n | pos2(:,1)==1 | pos2(:,1)==n;
-    %final_pos1(z) = pos1(z,1);
-    %final_pos2(z) = pos2(z,1);
     q = sub2ind([n n],pos1(z,2),pos2(z,2));
     reward(q) = reward(q) + g(sub2ind([n n],pos1(z,1),pos2(z,1)));
     pos1(z,:) = [];
@@ -53,7 +45,6 @@ while size(pos1,1) >= 1
     j = j + 1;
     r = size(pos1,1);
 end
-%R_avg = sum(g(sub2ind([n n],final_pos1,final_pos2)),3)/m;
 R_avg = reward/m;
 u(2:n-1,2:n-1) = R_avg(2:n-1,2:n-1);
 mesh(u)

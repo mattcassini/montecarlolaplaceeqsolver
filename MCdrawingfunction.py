a75d310b3bace099cb5ab93380759ee2d31f62ca
Moistman2222 <mdepena22@gmail.com>
2022-11-19 21:14:29 -0500

    A Monte Carlo sim with gui that asks desired grid size, works  based off of the function to run a Monte Carlo sim

--- MCdrawingfunction.py
+++ MCdrawingfunction.py
@@ -4,124 +4,127 @@ import tkinter
 import numpy as ny
 
 
-def MonteCarloVis(size):
-    delta=380*2/(size-1)
-    #used for drawing grid
-    pencil=turtle.Turtle()
-    pencil.hideturtle()
-    pencil.penup()
-    pencil.speed(speed=0)
-    pencil.goto(-380,380)
-    pencil.pendown()
-    pencil.setheading(270)
-    
+def MonteCarloVis(size,marker,node,length,width):
+    delta1=length/(size-1)
+    delta2=width/(size-1)
     #position matrix set up
     positions_matrix=ny.zeros([size,size])
-    
+    marker.speed(speed=0)
     for i in range(1,size-1):
         for j in range(1,size-1):
             positions_matrix[i][j]=1
     row=random.randint(2,size-2)
     col=random.randint(2,size-2)
     #node we will watch
-    node=turtle.Turtle()
-    node.hideturtle()
+    #node.hideturtle()
     node.penup()
-    node.goto(-380,380)
     node.pencolor("blue")
     node.shape("circle")
     node.speed(4)
     node.turtlesize(0.85)
     node.width(6)
-
-    node.setheading(0)
-    node.forward(col*delta)
-    node.setheading(270)
-    node.forward(row*delta)
-    node.setheading(0)
-    node.pendown()
+    node.hideturtle()
     # makes a square
-    for i in range(4):
-        pencil.forward(2*380)
-        pencil.left(90)
+    marker.hideturtle()
+    marker.forward(length)
+    marker.right(90)
+    marker.forward(width)
+    marker.right(90)
+    marker.forward(length)
+    marker.right(90)
+    marker.forward(width)
+    marker.right(90)
+    marker.speed(0)
     
     # makes the gridlines
+    
     if size%2==0:
+        for i in range(int(size/2)-1):
+            marker.forward(delta1)
+            marker.setheading(270)
+            marker.forward(width)
+            marker.setheading(0)
+            marker.forward(delta1)
+            marker.setheading(90)
+            marker.forward(width)
+            marker.setheading(0)
+        marker.forward(delta1)
+        marker.setheading(270)
+        
         for j in range(int(size/2)-1):
-            pencil.forward(delta)
-            pencil.setheading(0)
-            pencil.forward(2*380)
-            pencil.right(90)
-            pencil.forward(delta)
-            pencil.setheading(0)
-            pencil.backward(380*2)
-            pencil.setheading(270)
-        pencil.forward(delta)
-        pencil.setheading(0)
-        for k in range(int(size/2)-1):
-            pencil.forward(delta)
-            pencil.setheading(90)
-            pencil.forward(2*380)
-            pencil.setheading(0)
-            pencil.forward(delta)
-            pencil.setheading(90)
-            pencil.backward(2*380)
-            pencil.setheading(0)
-
-
-             
+            marker.forward(delta2)
+            marker.setheading(180)
+            marker.forward(length)
+            marker.setheading(270)
+            marker.forward(delta2)
+            marker.setheading(0)
+            marker.forward(length)
+            marker.setheading(270)
+        
+        
         
     else:
-         for j in range(int(size)-2):
-            pencil.forward(delta)
-            pencil.setheading(0)
-            pencil.forward(2*380)
-            pencil.backward(2*380)
-            pencil.setheading(270)
-         pencil.forward(delta)
-         pencil.setheading(0)
-         for k in range(int(size/2)):
-            pencil.forward(delta)
-            pencil.setheading(90)
-            pencil.forward(2*380)
-            pencil.setheading(0)
-            pencil.forward(delta)
-            pencil.setheading(90)
-            pencil.backward(2*380)
-            pencil.setheading(0)
+        for m in range(int(size/2)):
+            marker.forward(delta1)
+            marker.setheading(270)
+            marker.forward(width)
+            marker.setheading(0)
+            marker.forward(delta1)
+            marker.setheading(90)
+            marker.forward(width)
+            marker.setheading(0)
+        marker.setheading(270)
+        
+        for k in range(int(size/2)):
+            marker.forward(delta2)
+            marker.setheading(180)
+            marker.forward(length)
+            marker.setheading(270)
+            marker.forward(delta2)
+            marker.setheading(0)
+            marker.forward(length)
+            marker.setheading(270)
     
-    
-    
-    #puts the node in a random position that is not within the first 3 layers
-    #shouldnt use grids that are too small
-    
-    node.showturtle() #makes the node visable
-    run=1
-    while run==1:
+    #picks a random node to run a montecarlo simulation on
+    row=random.randint(1,size-2)
+    col=random.randint(1,size-2)
+    node.speed(speed=0)
+    node.setheading(270)
+    node.forward(delta2*row)
+    node.setheading(0)
+    node.forward(delta1*col)
+    node.showturtle()
+    node.speed(speed=0.9)
+    node.pendown()
+    run=True
+    while run==True:
         r=random.random()
-        if r<= 0.25:
-            row-=1 #move up
+        if positions_matrix[row][col]==0:
+            break
+        if r<=0.25:
+            row-=1
             node.setheading(90)
-            node.forward(delta)
+            node.forward(delta2)
         elif r>0.25 and r<=0.5:
-            row+=1 #move down
+            row+=1
             node.setheading(270)
-            node.forward(delta)
-        elif r>0.5 and r<= 0.75:
-            col-=1 #move left
+            node.forward(delta2)
+        elif r>0.5 and r<=0.75:
+            col-=1
             node.setheading(180)
-            node.forward(delta)
+            node.forward(delta1)
         else:
-            col+=1 #move right
+            col+=1
             node.setheading(0)
-            node.forward(delta)
-        if int(positions_matrix[row][col])==0:
-            run=-1
-         
-    Screen=turtle.Screen()
-    Screen.exitonclick()
-   
+            node.forward(delta1)
+        
 
+
+
+        
+            
     
-
-MonteCarloVis(25)
+    
+            
+    
+   

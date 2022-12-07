4d23f2b79e374d2241d8525a3754458450263926
Marissah McNeil <marissahmcneil@Marissahs-MacBook-Pro.local>
2022-12-07 11:54:43 -0500

    added contour code

--- equpotentiallines.m
+++ equpotentiallines.m
@@ -1,13 +1,13 @@
-x=linspace(1,7,500);
-y=linspace(1,9,500);
+x=linspace(0,7,200);
+y=linspace(0,9,200);
 [x,y]=meshgrid(x,y);
 
-uo=(48./sinh(9.*pi/7)).*sin(x.*pi./7).*sinh(y.*pi./7);
+uo=(35./sinh(9.*pi/7)).*sin(x.*pi.*i./7).*sinh(y.*pi.*i./7);
 usol=0;
  for i=2:100
-     un=48./sinh(9.*pi.*i./7).*sin(x.*pi.*i./7).*sinh(y.*pi.*i./7);
+     un=35./sinh(9.*pi.*i./7).*sin(x.*pi.*i./7).*sinh(y.*pi.*i./7);
      usol=un+uo;
      uo=un;
  end
  
- contour(x,y,usol)
+surf(x,y,usol)

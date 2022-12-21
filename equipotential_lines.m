33328ea37eeaace63ab81533d766516b16a5100f
Matt <mc225@njit.edu>
2022-12-21 09:54:14 -0500

    added error plots

--- equipotential_lines.m
+++ equipotential_lines.m
@@ -8,11 +8,15 @@ y=linspace(0,9,n);
 
 u = @(x,y,i) (48./sinh(9*pi*i/7)).*sin(x*pi*i/7).*sinh(y*pi*i/7)/(i*pi);
 
-usol=0;
+usol=zeros(n,n);
+usol(1,:) = 0;
+usol(:,1) = 0;
+usol(:,n) = 0;
+usol(n,:) = 12;
 
 for i=1:2:180
     un = u(x,y,i);
-    usol= usol + un;
+    usol(2:end-1,2:end-1) = usol(2:end-1,2:end-1) + un(2:end-1,2:end-1);
 end
  
 %  contour(usol);
--- error_convergence.m
+++ error_convergence.m
@@ -1,15 +1,15 @@
 %Error Between Monte Carlo Solution and Analytical Solution
 clear;close all;
-n = [8 25 50 75 100];
+n = [8 25 50 75 100 125 150];
 m = [100 1000];
 for i = 1:length(n)
-    u_exact = equipotential_lines(n(i));
+    u_exact = equipotential_lines(n(i))';
     for j = 1:length(m)
         u_approx = tour_du_wino(n(i),m(j));
+        err = abs(u_exact - u_approx);
+        maxerr(i,j) = max(err(:));
+        L1err(i,j) = mean(err(:),'omitnan');
     end
-    err = abs(u_exact - u_approx);
-    maxerr(i,j) = max(err(:));
-    L1err(i,j) = mean(err(:));
 end
 
 load gong
